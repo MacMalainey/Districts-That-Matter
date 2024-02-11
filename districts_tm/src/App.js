3f7d4e886a38a3ed6bd3f74b0037c92f2f21686b
Harsh <hgshah@ualberta.ca>
2024-02-10 16:54:10 -0800

    initial

---
+++ districts_tm/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>ECE 493 Capstone: Districts That Matter </p>
+        <p> Visualizer under development .....</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
