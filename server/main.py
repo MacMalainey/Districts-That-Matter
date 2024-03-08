dd2d275655de04dc4e4e9ec811f252e6a84b85ce
Mackenzie Malainey <mmalaine@ualberta.ca>
2024-03-08 16:32:25 -0700

    server: add api endpoint for boundary data

--- server/main.py
+++ server/main.py
@@ -1,7 +1,21 @@
-from flask import Flask
+import geojson
 
-app = Flask(__name__)
+from . import queries
+from . import app
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+###################
+## API ENDPOINTS ##
+###################
+
+@app.route("/api/units/all")
+def api_units_all():
+    return geojson.dumps(queries.query_geodata(None), indent=2)
+
+@app.route("/api/units/<unit>/demographics")
+def api_units_demographics():
+    pass
+    
