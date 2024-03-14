2354a4ff6490b76db62d3ec09988293ce2840f6d
Harsh <hgshah@ualberta.ca>
2024-03-14 12:42:54 -0700

    color palette in progress

--- districts_tm/src/Sidebar.js
+++ districts_tm/src/Sidebar.js
@@ -1,20 +1,43 @@
-import React from 'react'
+import {React, useState} from 'react'
 import './Sidebar.css'; // Import your CSS file
+import {SketchPicker} from 'react-color'
 
 function Sidebar() {
+    const [selectpaint, setselectpaint] = useState(false)
+    const [selectcursor, setselectcursor] = useState(false)
+
     const submit = () => {
         
         window.location.reload();
     }
 
+    const handlepaint = () =>{
+        setselectpaint(true)
+        console.log("paint is selected")
+    }
+    const handlecursor = ()=>{
+        setselectpaint(false)
+        setselectcursor(true)
+    }
+    const colordata = () =>{
+        alert('data sent')
+    }
   return (
     
         <div className = 'sidebar'>
+            
+            <FaPaintBrush onClick={handlepaint} color="grey"/>
+            <FaHandPaper onClick={handlecursor} color = "grey"/> <br></br>
+            <SketchPicker/>
             <input type='text' placeholder='Update District Number'/> <br/>
-            <button type='submit' onClick={submit}>submit</button>
+            <button type='submit' onClick={submit}>submit</button> <br></br>
+            <button type = 'submit' onClick={colordata}>Run Analysis</button>
+
+            <p>Data Layer</p>
+            <input type='checkbox'></input>
+            <label>Show painted region</label>
         </div>
        
-    
   )
 }
 
