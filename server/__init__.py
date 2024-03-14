cacea7d51c199eccf48f50dc847d79cbbb2ca67e
Harsh <hgshah@ualberta.ca>
2024-03-14 12:37:47 -0700

    server main.py changed for CORs

--- server/__init__.py
+++ server/__init__.py
@@ -1,6 +1,8 @@
 import json
 
 from flask import Flask
+from flask_cors import CORS
+
 from . import queries
 
 schema: dict[str, list[str]]
@@ -8,5 +10,7 @@ schema: dict[str, list[str]]
 with open("../.local/dtmTORO_schema.json") as schema_file:
     schema = json.load(schema_file)
 
+
 app = Flask(__name__)
+CORS(app)
 queries.init_app(app)
--- server/main.py
+++ server/main.py
@@ -4,6 +4,7 @@ from . import schema
 
 from flask import request
 
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
