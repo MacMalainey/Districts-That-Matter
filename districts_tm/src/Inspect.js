ad63e8733e2fe05d125717102a5fc9164e46f851
Harsh <hgshah@ualberta.ca>
2024-03-19 13:10:17 -0700

    color palette added + onselect color changes

--- districts_tm/src/Inspect.js
+++ districts_tm/src/Inspect.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
 function Inspect() {
+    const mapid = localStorage.getItem('mapid')
+    
+    
   return (
     <div>
       <p> This is a Inspect Layer</p>
       
-      
-      
+
     </div>
   )
 }
