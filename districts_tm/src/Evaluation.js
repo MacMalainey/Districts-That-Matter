7e263f43570493addc9949f26b8037cd3d64d529
Harsh <hgshah@ualberta.ca>
2024-03-28 15:50:15 -0700

    comparision for districts demo data in progress...

--- districts_tm/src/Evaluation.js
+++ districts_tm/src/Evaluation.js
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import axios, { all } from 'axios';
 import { click } from '@testing-library/user-event/dist/click';
-
+import { Chart } from 'chart.js';
+let inspectdata = []
 function Evaluation() {
   // implemented FR 6 and FR7 
   // district number is taken as user input and we recalculate population range per district
   const [totalpop, settotalpop] = useState(null)
+  const [districtdemo, setdistrictdemo] = useState(null)
   const totaldistrict = localStorage.getItem('TotalDistricts')
   const [selectTP, setselectTP] = useState(false)
+  const [selectDD, setselectDD] = useState(false)
+  const [selectpop, setselectpop] = useState(false)
+  
   useEffect(()=>{
     const handlepopulation = async () =>{
       const response = await axios.get('http://127.0.0.1:5000/api/units/totals')
@@ -18,8 +23,58 @@ function Evaluation() {
     handlepopulation();
   }, [totalpop])
   
+
+  useEffect(()=>{
+    const handledistrictdemo = async () =>{
+      const response = await axios.get('http://127.0.0.1:5000/api/districts/demographics')
+      const inspectDD = response.data
+      for (const k in inspectDD){
+        
+        for (const j in inspectDD[k]){
+          inspectdata.push([k,j,inspectDD[k][j]])
+        }
+          // console.log(j, inspectDD[k][j])
+          // const val = inspectDD[k]
+          // inspectdata.push([k,val])
+        
+          
+          
+
+        
+       
+        
+      }
+      
+      console.log(inspectdata)
+    }
+    handledistrictdemo();
+   
+  }, [])
   
- 
+  const handleinspectDD = (pop) => {
+    const filteredinspect = inspectdata.filter((arr)=>arr[1]===pop)
+    console.log(filteredinspect)
+    return <p>Population 
+       <input type='checkbox' value={selectpop} onChange={() => setselectpop(!selectpop)}></input>
+       {selectpop && <ul>
+        
+        {filteredinspect.map((charact,indexvalue)=>(<li key={indexvalue}>
+          <table>
+            <thead>
+              <tr>
+                District {charact[0]} : {charact[1]} is {charact[2]}
+              </tr>
+              
+            </thead>
+          
+          </table>
+          
+        </li>))}
+      
+      </ul>}
+    </p>
+    
+  }
   
   return (
     <div>
@@ -29,7 +84,9 @@ function Evaluation() {
       {selectTP && <p>total population is {totalpop} <br></br>
       updated total population per district is: {Math.floor(totalpop/totaldistrict)}
       </p>}
-      
+      <label>Compare District's</label>
+      <input type='checkbox' value={selectDD} onChange={() =>setselectDD(!selectDD)}></input>
+      {selectDD && <br></br> && handleinspectDD('population')}
       
     </div>
   )
