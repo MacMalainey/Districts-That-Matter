3d2ec4f18594ac81ace174180dd4b49855d2c908
Harsh <hgshah@ualberta.ca>
2024-03-31 21:31:51 -0700

    population analysis bar added

--- districts_tm/src/Evaluation.js
+++ districts_tm/src/Evaluation.js
@@ -181,18 +181,18 @@ function Evaluation() {
 
     
     </select>
-     <p> with </p> 
+     {/* <p> with </p> 
     <select value={secondcharact} onChange={(e)=>setsecondcharact(e.target.value)}>
     <option style={{fontStyle:'italic'}}> select...</option>
   
     {inspectdata.map((charact,indexvalue) =>(
       <option key={indexvalue} value={charact[1]}>{charact[1]}</option>
-    ))}
+    ))} */}
 
     
-    </select>
+    {/* </select> */}
        
-       {selectcharact && secondcharact && <p>
+       {selectcharact  && <p>
         <table> 
         <tr>
               <th> DN  </th> 
@@ -204,26 +204,30 @@ function Evaluation() {
               <th></th>
               <th></th>
               <th>{secondcharact}</th>
+              <th>Progress</th>
             </tr >
 
        
         
         {addcolor.map((charact,indexvalue)=>(
 
-              <tr id='row1' key={indexvalue}>
+              <tr key={indexvalue}>
                 <td style={{background: charact[3],width:'35px', borderRadius:10, height:'15px', color:'black', paddingLeft:'20px'}}> {charact[0]} </td>
                 <td></td>
                 <td></td>
                 <td>{charact[2]}</td>
-                
+                <td></td>
+                <td></td>
+                <td></td>
+                <td><progress max={5000} value={charact[2]} > </progress></td>
               </tr>
 
         ))}
         
 
-        {filsecondcharact.map((charact,indexvalue)=>(
-
-          <tr id='row1'  key={indexvalue}>
+        {/* {filsecondcharact.map((charact,indexvalue)=>(
+          
+          <tr   key={indexvalue}>
             <td></td>
             <td></td>
             <td></td>
@@ -234,7 +238,186 @@ function Evaluation() {
             
           </tr>
 
-))}
+))} */}
+       </table>
+      </p>}
+    </p>
+    
+  }
+  
+  const handlepopulation = () => {
+    const filteredinspectcharact = inspectdata.filter((arr)=>arr[1]==='population')
+    const addcolor = filteredinspectcharact.map((chek)=> {
+      console.log(chek[0])
+      if(chek[0]==11){
+        return [...chek,'#ff1a1a']
+      }
+      else if(chek[0]==12){
+        return [...chek,'#ff8080']
+      }
+      else if(chek[0]==13){
+        return [...chek,'#ffb3b3']
+      }
+      else if(chek[0]==14){
+        return [...chek,'#ffe6e6']
+      }
+      else if(chek[0]==15){
+        return [...chek,'#ff0000']
+      }
+      else if(chek[0]==16){
+        return [...chek,'#b30000']
+      }
+      else if(chek[0]==17){
+        return [...chek,'#4d0000']
+      }
+      else if(chek[0]==18){
+        return [...chek,'#ff8000']
+      }
+      else if(chek[0]==19){
+        return [...chek,'#ffbf00']
+      }
+      else if(chek[0]==20){
+        return [...chek,'#ffff00']
+      }
+      else if(chek[0]==21){
+        return [...chek,'#bfff00']
+      }
+      else if(chek[0]==22){
+        return [...chek,'#80ff00']
+      }
+      else if(chek[0]==23){
+        return [...chek,'#40ff00']
+      }
+      else if(chek[0]==24){
+        return [...chek,'#00ff00']
+      }
+      else if(chek[0]==25){
+        return [...chek,'#00ff40']
+      }
+      else if(chek[0]==26){
+        return [...chek,'#00ff80']
+      }
+      else if(chek[0]==27){
+        return [...chek,'#00ffbf']
+      }
+      else if(chek[0]==28){
+        return [...chek,'#00ffff']
+      }
+      else if(chek[0]==29){
+        return [...chek,'#00bfff']
+      }
+      else if(chek[0]==30){
+        return [...chek,'#0080ff']
+      }
+      else if(chek[0]==31){
+        return [...chek,'#0040ff']
+      }
+      else if(chek[0]==32){
+        return [...chek,'#0000ff']
+      }
+      else if(chek[0]==33){
+        return [...chek,'#4000ff']
+      }
+      else if(chek[0]==34){
+        return [...chek,'#8000ff']
+      }
+      else if(chek[0]==35){
+        return [...chek,'#bf00ff']
+      }
+      else if(chek[0]==36){
+        return [...chek,'#ff00ff']
+      }
+      else if(chek[0]==37){
+        return [...chek,'#ff00bf']
+      }
+      else if(chek[0]==38){
+        return [...chek,'#ff0080']
+      }
+      else if(chek[0]==39){
+        return [...chek,'#ff0040']
+      }
+      else if(chek[0]==40){
+        return [...chek,'#996680']
+      }
+      else if(chek[0]==41){
+        return [...chek,'#80c4b7']
+      }
+      else if(chek[0]==42){
+        return [...chek,'#eeeeee']
+      }
+      else if(chek[0]==43){
+        return [...chek,'#feffba']
+      }
+      else if(chek[0]==44){
+        return [...chek,'#c90076']
+      }
+      else if(chek[0]==45){
+        return [...chek,'#073763']
+      }
+      else if(chek[0]==46){
+        return [...chek,'#669999']
+      }
+      else if(chek[0]==47){
+        return [...chek,'#ffa500']
+      }
+      else if(chek[0]==48){
+        return [...chek,'#476b6b']
+      }
+      else if(chek[0]==49){
+        return [...chek,'#854442']
+      }
+      else if(chek[0]==50){
+        return [...chek,'#f24e70']
+      }
+    })
+    
+    return <p> Population analysis  
+      
+     {/* <p> with </p> 
+    <select value={secondcharact} onChange={(e)=>setsecondcharact(e.target.value)}>
+    <option style={{fontStyle:'italic'}}> select...</option>
+  
+    {inspectdata.map((charact,indexvalue) =>(
+      <option key={indexvalue} value={charact[1]}>{charact[1]}</option>
+    ))} */}
+
+    
+    {/* </select> */}
+       
+       {<p>
+        <table> 
+        <tr>
+              <th> DN  </th> 
+              <th></th>
+              <th></th>
+              <th>
+                Population
+              </th>
+              <th></th>
+              <th></th>
+              <th>{secondcharact}</th>
+              <th>Analysis</th>
+            </tr >
+
+       
+        
+        {addcolor.map((charact,indexvalue)=>(
+
+              <tr key={indexvalue}>
+                <td style={{background: charact[3],width:'35px', borderRadius:10, height:'15px', color:'black', paddingLeft:'20px'}}> {charact[0]} </td>
+                <td></td>
+                <td></td>
+                <td>{charact[2]}</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td><progress   max={125810} value={charact[2]} > </progress></td>
+              </tr>
+
+        ))}
+        
+
+        
        </table>
       </p>}
     </p>
@@ -249,12 +432,11 @@ function Evaluation() {
       
       <label>Show Total Population</label>
       <input type='checkbox' value={selectTP} onChange={() =>setselectTP(!selectTP)}></input>
-      {/* <p> {localStorage.getItem('population')}</p> */}
-      {selectTP && <p>total population is  <br></br>
-      
-      </p>}
       <label>Compare District's</label>
       <input type='checkbox' value={selectDD} onChange={() =>setselectDD(!selectDD)}></input>
+      {/* <p> {localStorage.getItem('population')}</p> */}
+      {selectTP && handlepopulation()}
+      
       {selectDD && <br></br> && handleinspectChar()}
       
     </div>
